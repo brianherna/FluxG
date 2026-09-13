0406acdf4554d6713a8fe21b6dab212d8c11edb1
brianherna <148389821+brianherna@users.noreply.github.com>
2026-09-13 01:32:09 -0600

    chore: add Spring Boot and MongoDB base

---
+++ src/main/java/com/fluxguard/FluxGApplication.java
@@ -0,0 +1,12 @@
+package com.fluxguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxGApplication.class, args);
+    }
+}
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,1 @@
+console.log('FluxG iniciado correctamente.');
